db79b8226f22c6574dea5ec62c65467feba728d3
kaipapar <120021905+kaipapar@users.noreply.github.com>
2024-01-13 19:33:50 +0200

    4.4 done
    test cases tested with one blog entry and multiple, gives correct results.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,15 @@
 const dummy = (blogs) => {
     return 1
 }
-  
-  module.exports = {
-    dummy
+
+
+const totalLikes = (blogs) => {
+    const initialValue = 0;
+    return blogs.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.likes,
+        initialValue,
+        );
+}
+module.exports = {
+    dummy, totalLikes
   }
