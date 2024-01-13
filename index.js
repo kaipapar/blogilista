39a28dddb81293531d404e8140a4545610e9a62f
kaipapar <120021905+kaipapar@users.noreply.github.com>
2024-01-13 18:32:48 +0200

    4.2 in progress. works. model and router put into its own file.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const blog = require('./models/blog')
-
+const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 require('dotenv').config()
 const http = require('http')
@@ -16,28 +16,7 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-  })
-
-app.get('/api/blogs', (request, response) => {
-  blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
+app.use(blogsRouter)
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
