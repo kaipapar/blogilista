c0e4f03dff14158770de40f980716dcc33d6b73f
kaipapar <120021905+kaipapar@users.noreply.github.com>
2024-01-25 17:07:53 +0200

    4.17 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user.js')
 /* 
 blogsRouter.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -14,7 +15,7 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findById(body.userId)
+  const user = await User.findById("65b23f507ca7fe5a6c8812a5")
   const blog = new Blog({
     title: body.title,
     author: body.author,
@@ -23,7 +24,7 @@ blogsRouter.post('/', async (request, response) => {
     user: user._id
   })
   const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedNote._id)
+  user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
   response.json(savedBlog)
 })
